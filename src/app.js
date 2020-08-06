932ac9e3cf611a3d21a55e56b32654f5f34abfd2
Marzeu <marzahell@hotmail.com>
2020-08-05 22:30:47 -0300

    rotas basicas criadas

--- src/app.js
+++ src/app.js
@@ -4,35 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//Carregar as Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-let route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
-    });
-});
-
-let create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-let put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-let del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
