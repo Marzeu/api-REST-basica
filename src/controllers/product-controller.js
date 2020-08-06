7e31161b1856dc1d9afb1d24ce85789d7be7707f
Marzeu <marzahell@hotmail.com>
2020-08-06 15:27:34 -0300

    filtros criados

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,44 @@ const { Mongoose } = require("mongoose");
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        })
+        .catch(e => {
+            res.status(400).send(e);
+        });
+}
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
 router.post('/', controller.post);
-router.put('/:id', controller.put);  
-router.delete('/', controller.delete);  
+router.put('/:id', controller.put);
+router.delete('/', controller.delete);
 
 module.exports = router;
