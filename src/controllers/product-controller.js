22c9c25ea4bcad6ee896e0d3e7a0cca8821e0f27
Marzeu <marzahell@hotmail.com>
2020-08-06 15:45:52 -0300

    metodo de atualizar produtos refinado

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -73,11 +73,26 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                slug: req.body.slug
+            }
+        })
+        .then(x => {
+            res.status(200).send({
+                message: 'Produto atualizado com sucesso!'
+            });
+        })
+        .catch(e => {
+            res.status(400).send({
+                message: 'Falha ao atualizar o produto',
+                data: e
+            });
+        });
 };
 
 exports.delete = (req, res, next) => {
