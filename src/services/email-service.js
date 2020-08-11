a35bcb8e776bdfeacb997b42fe883b65d0b44ab9
Marzeu <marzahell@hotmail.com>
2020-08-11 16:16:53 -0300

    email mais ou menos funcional

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,20 +1,21 @@
-// const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail');
 
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// exports.send = async (to, subject, body) => {
-//     sgMail
-//         .send({
-//             to: to,
-//             from: 'marzahell@hotmail.com',
-//             subject: 'asda',
-//             html: body
-//         })
-//         .then(() => { }, error => {
-//             console.error(error);
+exports.send = async (to, subject, body) => {
+    sgMail
+        .send({
+            to: to,
+            from: 'marzahell@hotmail.com',
+            subject: 'asda',
+            text: 'and easy to do anywhere, even with Node.js',
+            html: body
+        })
+        .then(() => { }, error => {
+            console.error(error);
 
-//             if (error.response) {
-//                 console.error(error.response.body)
-//             }
-//         });
-// } 
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        });
+} 
