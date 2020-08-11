65a942951f9307dc928418a7f3b4ddc224717b4e
Marzeu <marzahell@hotmail.com>
2020-08-11 17:27:51 -0300

    reparos na autorização

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
-const authService = require('../services/auth-service')
+const authService = require('../services/auth-service');
 
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
 router.get('/admin/:id', controller.getById);
 router.get('/tags/:tag', controller.getByTag);
 router.post('/', authService.authorize, controller.post);
-router.put('/:id', controller.put);
-router.delete('/', controller.delete);
+router.put('/:id', authService.authorize, controller.put);
+router.delete('/', authService.authorize, controller.delete);
 
 module.exports = router;
