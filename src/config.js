3f70d97ab90c2f8f192998154ee6b76a01458d94
Marzeu <marzahell@hotmail.com>
2020-08-12 17:30:06 -0300

    ...

---
+++ src/config.js
@@ -0,0 +1,10 @@
+global.SALT_KEY = 'C>s$m*q0ASd,/rMNtm^_6l]ra#+E]^t|,DuD]]r@VX{VZ3+*Q$t&zH+=Vob5F,xf';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://marza:12345@nodestore.uauvo.azure.mongodb.net/nodeStore?retryWrites=true&w=majority',
+    // sendgridKey: '',
+    containerConnectionString: 'sua conection string do azure storage'
+}
+
+
