9b294e3291e07c9c86c99b324337c09ab902fef2
Marzeu <marzahell@hotmail.com>
2020-08-13 19:14:06 -0300

    refresh-token

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -1,3 +1,4 @@
+'use strict';
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
@@ -52,6 +53,40 @@ exports.authenticate = async (req, res, next) => {
         }
 
         const token = await authService.generateToken({
+            id: customer._id,
+            email: customer.email,
+            name: customer.name
+        });
+
+        res.status(201).send({
+            token: token,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: "Falha ao processar sua requisição"
+        });
+    }
+};
+
+exports.refreshToken = async (req, res, next) => {
+    try {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        const data = await authService.decodeToken(token);
+
+        const customer = await repository.getById(data.id);           
+
+        if (!customer) {
+            res.status(404).send({
+                message: 'Cliente não encontrado'
+            });
+            return;
+        }
+
+        const tokenData = await authService.generateToken({
             id: customer._id,
             email: customer.email,
             name: customer.name
--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -1,3 +1,4 @@
+'use strict';
 const mongoose = require('mongoose');
 const Customer = mongoose.model('Customer');
 
@@ -11,5 +12,10 @@ exports.authenticate = async (data) => {
         email: data.email,
         password: data.password
     });
+    return res;
+}
+
+exports.getById = async (id) => {
+    const res = await Customer.findById(id);
     return res;
 }
